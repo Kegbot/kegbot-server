5e5c2026f92d8b150840e4f0eee98e6318047ddb
mike <opensource@hoho.com>
2006-01-09 02:25:27 +0000

    css file passthru

--- kegbotweb/src/css.php
+++ kegbotweb/src/css.php
@@ -3,11 +3,14 @@
    include_once('includes/SmartyBeer.class.php');
 
    $smarty = new SmartyBeer();
-   $file = $_GET['css'];
+   $file = pathinfo($_GET['css']);
+   $file = $file['basename'];
    $cid = $file;
 
    //$smarty->fetch("css.tpl",$cid);
-   $f = $smarty->basedir . '/css/' . realpath($file);
-   echo implode('', file($f));
+   $f = $smarty->basedir . '/css/' . $file;
+   if (file_exists($f)) {
+      echo implode('', file($f));
+   }
 
 ?>

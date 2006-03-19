6e36752633122b492956b1ad4c913092894dd756
mike <opensource@hoho.com>
2006-03-19 20:18:27 +0000

    - fixed dummy client

--- pykeg/scripts/client.py
+++ pykeg/scripts/client.py
@@ -2,7 +2,7 @@ import xmlrpclib
 s = xmlrpclib.ServerProxy('http://localhost:9966')
 while 1:
   user = raw_input('start flow: drinker? ').strip()
-  s.authFlow(user)
+  s.AuthUser(user)
   raw_input('stop flow by hitting enter...')
-  s.stopFlow()
+  s.StopFlow()
   print ''

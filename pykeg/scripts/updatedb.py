1f673dd13b0c135aa9c0d31cdc8d9c6cd73990d4
mike <opensource@hoho.com>
2006-04-10 01:56:45 +0000

    - cleaning up a bit

--- pykeg/scripts/updatedb.py
+++ pykeg/scripts/updatedb.py
@@ -13,43 +13,51 @@ dbdb   = config.get('DB','db')
 dbpass = config.get('DB','password')
 dbconn = MySQLdb.connect(host=dbhost, user=dbuser, passwd=dbpass, db=dbdb)
 
+
+### UPDATES TABLE
+
 def GetInstalledSchema():
    """ determine current schema version. quite stupid for now """
    c = dbconn.cursor()
-   q = """SELECT `value` from `config` where `key`='db.schema_version'"""
+   q = """SELECT `value` from `config` where `id`='db.schema_version'"""
    c.execute(q)
    r = c.fetchone()
    return r[0]
 
-def GetLatestSchema():
-   """ determine what version of kegbot.sql we have """
-   f = open('kegbot.sql')
-   firstline = f.readline()
-   f.close()
-   if not firstline.startswith('-- KEGBOT_SCHEMA_VERSION'):
-      return -1
-   k, v = firstline.strip().split('=')
-   v = int(v)
-   return v
+# convention: SchemaUpdate__X contains all necessary operations (if any) to
+# upgrade a DB schema from version X-1 to version X
+UPGRADE_PASS = 0
+UPGRADE_FAIL = 1
 
-def BuildConfig():
-   """ build mysql config table from a ConfigParser object """
-   for section in config.sections():
-      print section
-      for option in config.options(section):
-         k = '%s.%s' % (section, option)
-         k = k.lower()
-         v = config.get(section, option)
-         k = MySQLdb.escape_string(k)
-         v = MySQLdb.escape_string(v)
-         if section != 'DB':
-            c.execute("""INSERT INTO `config` (`key`,`value`) VALUES ('%s', '%s') """ % (k, v))
+class SchemaUpdate__2:
+   """ upgrade to schema 2 """
+   def Upgrade(self):
+      return UPGRADE_PASS
+
+class SchemaUpdate__3:
+   """ upgrade to schema 3 """
+   def Upgrade(self):
+      return UPGRADE_PASS
+
+def doUpgrade(current, latest):
+   for new_schema in range(current+1, latest+1):
+      print '>>> Upgrading to schema %i' % new_schema
+
+      ret = 0
+      cls = 'SchemaUpdate__%i' % new_schema
+      if not globals().has_key(cls):
+         print '!!! No upgrade found, assuming OK'
+         return
+
+      updater = globals()[cls]()
+      ret = updater.Upgrade()
+      if ret != UPGRADE_PASS:
+         print '!!! schema upgrade failed'
+         sys.exit(1)
+      print '<<< Upgrade to schema %i done' % new_schema
+      print ''
 
 def Main():
-
-   # print latest schema
-   latest = int(GetLatestSchema())
-   print 'Latest available schema:    %i' % latest
 
    # print current schema
    try:
@@ -61,10 +69,13 @@ def Main():
 
    print ''
 
-   if latest == current:
+   latest = 4
+   if current == latest:
       print 'Nothing to do, great!'
+      sys.exit(0)
    else:
-      print 'Shoot, work to do..'
+      doUpgrade(current, latest)
+      print 'Upgrade complete'
 
 
 if __name__ == '__main__':

8656ebe615b1ac3d6ab1c9ccdc937f630f89da9e
mike <opensource@hoho.com>
2007-02-26 01:30:03 +0000

    - initial db settings - module init file

---
+++ pykeg/db_settings.py
@@ -0,0 +1,4 @@
+# This a django settings module. The database needs to be configured in this file.
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = 'kegbot.db'
+AUTH_PROFILE_MODULE = "pykeg.UserProfile"

d61bcdbda56adfc04dff790dc0ea3a140896812a
mike <opensource@hoho.com>
2008-10-18 23:21:34 +0000

    Bump up minimum volume thresholds.

--- pykeg/core/kb_common.py
+++ pykeg/core/kb_common.py
@@ -2,11 +2,11 @@ from pykeg.core import util
 
 ### Drink-related constants
 
-# Don't record null drinks
-MIN_VOLUME_TO_RECORD = 1
+# Don't record teeny drinks
+MIN_VOLUME_TO_RECORD = 10
 
 # Don't show small drinks
-MIN_VOLUME_TO_DISPLAY = 1
+MIN_VOLUME_TO_DISPLAY = 10
 
 # Idle seconds allowed before a user gets booted for inactivity
 FLOW_IDLE_TIMEOUT = 30

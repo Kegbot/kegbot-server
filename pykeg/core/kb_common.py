a4bc8d7a05ee235a062c96b8351354e53060e44a
mike <opensource@hoho.com>
2009-04-12 20:13:09 +0000

    Add many new constants in kb_common
    (Most of these are not yet used; see future commits..)

--- pykeg/core/kb_common.py
+++ pykeg/core/kb_common.py
@@ -19,6 +19,18 @@ DRINK_SESSION_TIME_MINUTES = 90
 # 'concurrent' (see DrinkingSessionGroup table)
 GROUP_SESSION_TIME_MINUTES = 90
 
+# The maximum difference between consecutive meter readings that is considered
+# valid.
+MAX_METER_READING_DELTA = 2200*2
+
+# Address the kegnet server should bind to.
+#KEGNET_SERVER_BIND_ADDR = "127.0.0.1"   # listen on localhost only
+#KEGNET_SERVER_BIND_ADDR = ""            # listen on all network interface
+KEGNET_SERVER_BIND_ADDR = "127.0.0.1"
+
+# Port kegnet server should listen to
+KEGNET_SERVER_BIND_PORT = 9999
+
 ### Addresses and ports
 
 # Default port for JSON server
@@ -27,5 +39,40 @@ JSON_SERVER_PORT_DEFAULT = 9100
 ### Enums
 
 KB_EVENT = util.Enum(*(
-  'CHANNEL_ACTIVITY',
+  # Common events
+  'QUIT',
+  'START_COMPLETE',
+  'HEARTBEAT_1S',
+  'HEARTBEAT_10S',
+
+  'CLIENT_CONNECTED',
+  'CLIENT_DISCONNECTED',
+
+  # DrinkDatabaseService-generated events
+  'DRINK_CREATED',
+
+  # FlowManagerService handled events
+  'FLOW_DEV_REGISTER',
+  'FLOW_DEV_UNREGISTER',
+  'FLOW_DEV_ACTIVITY',
+  'FLOW_DEV_IDLE',
+
+  'FLOW_START',
+  'FLOW_UPDATE',
+  'FLOW_COMPLETE',
+  'FLOW_END',
+
 ))
+
+
+KB_DEVICE_TYPE = util.Enum(*(
+  'DEVICE_TYPE_UNKNOWN',
+  'DEVICE_TYPE_FLOW_DEV',
+  'DEVICE_TYPE_RELAY',
+  'DEVICE_TYPE_THERMO',
+))
+
+### Exceptions
+
+class ConfigurationError(Exception):
+  """Raised when the system is misconfigured"""

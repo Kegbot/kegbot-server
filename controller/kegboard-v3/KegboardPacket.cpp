6b3df4d54a284312ad7db3bd43724fcebe902a1d
mike <opensource@hoho.com>
2009-05-24 01:40:37 +0000

    Whitespace cleanup.

--- controller/kegboard-v3/KegboardPacket.cpp
+++ controller/kegboard-v3/KegboardPacket.cpp
@@ -30,15 +30,15 @@ uint16_t KegboardPacket::GenCrc()
   uint16_t crc=0;
   int i=0;
   int val;
-  
+
   crc = _crc_xmodem_update(crc, (m_type >> 8) & 0xff);
   crc = _crc_xmodem_update(crc, m_type & 0xff);
   crc = _crc_xmodem_update(crc, (m_len >> 8) & 0xff);
   crc = _crc_xmodem_update(crc, m_len & 0xff);
-  
+
   for (i=0; i<m_len; i++)
     crc = _crc_xmodem_update(crc, (int)(m_payload[i]) & 0xff);
-  
+
   return crc;
 }
 
@@ -50,11 +50,11 @@ void KegboardPacket::Print()
   Serial.print((m_type >> 8) & 0xff, BYTE);
   Serial.print(m_len & 0xff, BYTE);
   Serial.print((m_len >> 8) & 0xff, BYTE);
-  
+
   // payload
   for (i=0; i<m_len; i++)
     Serial.print(m_payload[i], BYTE);
-  
+
   // trailer
   uint16_t crc = GenCrc();
   Serial.print(crc & 0xff, BYTE);

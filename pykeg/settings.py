4b63a22f69f2aa6d99e633620ec66848472806c1
mike <opensource@hoho.com>
2009-08-22 13:29:03 -0700

    Remove some junk from pykeg settings.

--- pykeg/settings.py
+++ pykeg/settings.py
@@ -2,45 +2,6 @@
 # Note: YOU SHOULD NOT NEED TO EDIT THIS FILE.  Instead, see the instructions in
 # common_settings.py.example.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-# Local time zone for this installation. All choices can be found here:
-# http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
-TIME_ZONE = 'America/Los_Angeles'
-
-# Language code for this installation. All choices can be found here:
-# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-# http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.doc.XViewMiddleware',
-)
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates".
-    # Always use forward slashes, even on Windows.
-    "templates",
-)
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,9 +12,6 @@ INSTALLED_APPS = (
     'django.contrib.markup',
     'pykeg.core',
 )
-
-AUTH_PROFILE_MODULE = "core.UserProfile"
-LOGIN_REDIRECT_URL = "/account/"
 
 try:
   import common_settings

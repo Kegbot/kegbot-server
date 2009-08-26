ac671293c4154403ddf5bf1704c7a0a25e0aac4b
mike <opensource@hoho.com>
2009-08-25 22:35:42 -0700

    Add extra debug information to importhacks.

--- pykeg/bin/importhacks.py
+++ pykeg/bin/importhacks.py
@@ -21,36 +21,80 @@
 import os
 import sys
 
-### Extend PYTHONPATH so we can load settings from it
+_DEBUG = False
 
 SYSTEM_SETTINGS_DIR = "/etc/kegbot"
 HOME_DIR = os.environ.get('HOME')
 USER_SETTINGS_DIR = os.path.join(HOME_DIR, '.kegbot')
 
-# Greatest precedence: $HOME/.kegbot/
-if USER_SETTINGS_DIR not in sys.path:
-  sys.path.append(USER_SETTINGS_DIR)
+def _Debug(message):
+  if _DEBUG:
+    print 'importhacks:', message
 
-# Next precedence: /etc/kegbot
-if SYSTEM_SETTINGS_DIR not in sys.path:
-  sys.path.append(SYSTEM_SETTINGS_DIR)
+def _AppendToSysPath(path):
+  path = os.path.abspath(path)
+  if path not in sys.path:
+    _Debug('Adding to sys.path: %s' % path)
+    sys.path.append(path)
+  else:
+    _Debug('Already in sys.path: %s' % path)
 
-### Set django settings if not set
+def _ExtendSysPath():
+  """ Add some paths where we'll look for user settings. """
+  # Greatest precedence: $HOME/.kegbot/
+  # Next precedence: /etc/kegbot
+  for dir in (USER_SETTINGS_DIR, SYSTEM_SETTINGS_DIR):
+    _AppendToSysPath(dir)
+    if _DEBUG:
+      test_settings = os.path.join(dir, 'common_settings.py')
+      if os.path.exists(test_settings):
+        _Debug('%s exists' % test_settings)
+      else:
+        _Debug('%s does NOT exist' % test_settings)
 
-if not os.environ.get('DJANGO_SETTINGS_MODULE'):
-  os.environ['DJANGO_SETTINGS_MODULE'] = 'pykeg.settings'
+  # Also extend the path to pick up the pykeg/ parent dir.
+  # TODO(mikey): this may need to be reconsidered when installing from
+  # distutils.
+  _local_dir = os.path.dirname(sys.modules[__name__].__file__)
+  _package_dir = os.path.normpath(os.path.join(_local_dir, '../..'))
+  _AppendToSysPath(_package_dir)
 
-### Extend the python path if import fails
+  # Add pykeg/external to the path, too.
+  _external_dir = os.path.join(_package_dir, 'pykeg', 'external')
+  _AppendToSysPath(_external_dir)
 
-_local_dir = os.path.dirname(sys.modules[__name__].__file__)
-_package_dir = os.path.normpath(os.path.join(_local_dir, '../..'))
+def _SetDjangoSettingsEnv(settings='pykeg.settings'):
+  """ Set django settings if not set. """
+  if not os.environ.get('DJANGO_SETTINGS_MODULE'):
+    _Debug('Setting DJANGO_SETTINGS_MODULE=%s' % (settings,))
+    os.environ['DJANGO_SETTINGS_MODULE'] = settings
 
-# add external paths
-sys.path.append(os.path.join(_package_dir, 'pykeg', 'external'))
+def _FixAll():
+  _ExtendSysPath()
+  _SetDjangoSettingsEnv()
 
-try:
-  import pykeg
-except ImportError:
-  print 'Adding to PYTHONPATH:', _package_dir
-  sys.path.append(_package_dir)
+  if _DEBUG:
+    try:
+      import pykeg
+      pykeg_dir = os.path.dirname(pykeg.__file__)
+      _Debug('Pykeg loaded from dir: %s' % pykeg_dir)
+    except ImportError:
+      print 'Warning: pykeg could not be imported'
+    try:
+      import pykeg.external
+      external_dir = os.path.dirname(pykeg.external.__file__)
+      _Debug('Pykeg external loaded from dir: %s' % external_dir)
+    except ImportError:
+      print 'Warning: pykeg.external could not be imported'
+    try:
+      import common_settings
+      common_dir = os.path.dirname(common_settings.__file__)
+      _Debug('common_settings loaded from dir: %s' % common_dir)
+    except ImportError:
+      print 'Warning: common_settings could not be imported'
 
+if __name__ == '__main__' or os.environ.get('DEBUG_IMPORT_HACKS'):
+  # When run as a program, or DEBUG_IMPORT_HACKS is set: print debug info
+  _DEBUG = True
+
+_FixAll()

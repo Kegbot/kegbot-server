6644d050f1d322f5a0a68e6b49f6948fcb5a9b70
mike <opensource@hoho.com>
2009-09-06 19:05:38 -0700

    Add missing config class.

---
+++ pykeg/core/config.py
@@ -0,0 +1,39 @@
+# Copyright 2009 Mike Wakerly <opensource@hoho.com>
+#
+# This file is part of the Pykeg package of the Kegbot project.
+# For more information on Pykeg or Kegbot, see http://kegbot.org/
+#
+# Pykeg is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pykeg is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pykeg.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Miscellanous configuration from the database."""
+
+from pykeg.core import models
+
+class KegbotConfig:
+  def get(self, option, default=None):
+    try:
+      return models.Config.objects.get(key=option).value
+    except models.Config.DoesNotExist:
+      return default
+
+  def getboolean(self, option):
+    val = self.get(option, 'false')
+    if val.lower() == 'true' or val == '1':
+      return True
+    return False
+
+  def IsFeatureEnabled(self, feature_name):
+    key = '.'.join(('feature', feature_name, 'enable'))
+    return self.getboolean(key)
+

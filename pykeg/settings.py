20f60d5cdf95123ff1fa82ef1280055df705b9fe
mike <opensource@hoho.com>
2009-09-10 21:27:34 -0700

    More kegweb stuff needed in settings.

--- pykeg/settings.py
+++ pykeg/settings.py
@@ -14,10 +14,51 @@ INSTALLED_APPS = (
     'pykeg.contrib.twitter',
     'kegweb.kegweb',
     'kegweb.accounting',
+    'registration',
     'south',
 )
 
 AUTH_PROFILE_MODULE = "core.UserProfile"
+LOGIN_REDIRECT_URL = "/account/"
+
+### Kegweb specific stuff
+
+ROOT_URLCONF = 'kegweb.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates".
+    # Always use forward slashes, even on Windows.
+    "../kegweb/templates",
+)
+
+SITE_ID = 1
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT.
+# Example: "http://media.lawrence.com"
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+)
+
 
 try:
   import common_settings

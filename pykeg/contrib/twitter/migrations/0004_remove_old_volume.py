7332d171aa483b78272be301c7d18ee945dd30c4
mike <opensource@hoho.com>
2009-09-16 22:31:00 -0700

    Add missing migration from previous commit.

---
+++ pykeg/contrib/twitter/migrations/0004_remove_old_volume.py
@@ -0,0 +1,143 @@
+
+from south.db import db
+from django.db import models
+from pykeg.contrib.twitter.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Deleting field 'DrinkClassification.minimum_volume'
+        db.delete_column('twitter_drinkclassification', 'minimum_volume')
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Adding field 'DrinkClassification.minimum_volume'
+        db.add_column('twitter_drinkclassification', 'minimum_volume', orm['twitter.drinkclassification:minimum_volume'])
+        
+    
+    
+    models = {
+        'auth.group': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)"},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'core.beerstyle': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'core.beertype': {
+            'abv': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'brewer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Brewer']"}),
+            'calories_oz': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'carbs_oz': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'style': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.BeerStyle']"})
+        },
+        'core.brewer': {
+            'comment': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'distribution': ('django.db.models.fields.CharField', [], {'default': "'unknown'", 'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'origin_city': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'origin_country': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '128'}),
+            'origin_state': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '128'}),
+            'url': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'core.drink': {
+            'endtime': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'keg': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Keg']", 'null': 'True', 'blank': 'True'}),
+            'starttime': ('django.db.models.fields.DateTimeField', [], {}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'valid'", 'max_length': '128'}),
+            'ticks': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'volume_ml': ('django.db.models.fields.FloatField', [], {})
+        },
+        'core.keg': {
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'enddate': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'origcost': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'size': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.KegSize']"}),
+            'startdate': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.BeerType']"})
+        },
+        'core.kegsize': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'volume_ml': ('django.db.models.fields.FloatField', [], {})
+        },
+        'core.userlabel': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'labelname': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'core.userprofile': {
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'labels': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.UserLabel']"}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'}),
+            'weight': ('django.db.models.fields.FloatField', [], {})
+        },
+        'twitter.drinkclassification': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'minimum_volume_ml': ('django.db.models.fields.FloatField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        'twitter.drinkremark': {
+            'drink_class': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['twitter.DrinkClassification']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'remark': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        'twitter.drinktweetlog': {
+            'drink': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Drink']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tweet_log': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['twitter.TweetLog']"})
+        },
+        'twitter.tweetlog': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tweet': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'twitter_id': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        'twitter.usertwitterlink': {
+            'twitter_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'user_profile': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['core.UserProfile']", 'unique': 'True', 'primary_key': 'True'}),
+            'validated': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'})
+        }
+    }
+    
+    complete_apps = ['twitter']

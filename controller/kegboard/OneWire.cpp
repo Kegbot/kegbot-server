2c0ad3b22e8220ca5f3ad3a4dbe261a30d3dcb07
mike <opensource@hoho.com>
2009-12-27 10:16:57 -0800

    Whitespace/style cleanups.

--- controller/kegboard/OneWire.cpp
+++ controller/kegboard/OneWire.cpp
@@ -71,14 +71,14 @@ extern "C" {
 
 OneWire::OneWire( uint8_t pinArg)
 {
-    pin = pinArg;
-    port = digitalPinToPort(pin);
-    bitmask =  digitalPinToBitMask(pin);
-    outputReg = portOutputRegister(port);
-    inputReg = portInputRegister(port);
-    modeReg = portModeRegister(port);
+  pin = pinArg;
+  port = digitalPinToPort(pin);
+  bitmask =  digitalPinToBitMask(pin);
+  outputReg = portOutputRegister(port);
+  inputReg = portInputRegister(port);
+  modeReg = portModeRegister(port);
 #if ONEWIRE_SEARCH
-    reset_search();
+  reset_search();
 #endif
 }
 
@@ -90,24 +90,24 @@ OneWire::OneWire( uint8_t pinArg)
 // Returns 1 if a device asserted a presence pulse, 0 otherwise.
 //
 uint8_t OneWire::reset() {
-    uint8_t r;
-    uint8_t retries = 125;
+  uint8_t r;
+  uint8_t retries = 125;
 
-    // wait until the wire is high... just in case
-    pinMode(pin,INPUT);
-    do {
-	if ( retries-- == 0) return 0;
-	delayMicroseconds(2); 
-    } while( !digitalRead( pin));
-    
-    digitalWrite(pin,0);   // pull low for 500uS
-    pinMode(pin,OUTPUT);
-    delayMicroseconds(500);
-    pinMode(pin,INPUT);
-    delayMicroseconds(65);
-    r = !digitalRead(pin);
-    delayMicroseconds(490);
-    return r;
+  // wait until the wire is high... just in case
+  pinMode(pin,INPUT);
+  do {
+    if ( retries-- == 0) return 0;
+    delayMicroseconds(2);
+  } while( !digitalRead( pin));
+
+  digitalWrite(pin,0);   // pull low for 500uS
+  pinMode(pin,OUTPUT);
+  delayMicroseconds(500);
+  pinMode(pin,INPUT);
+  delayMicroseconds(65);
+  r = !digitalRead(pin);
+  delayMicroseconds(490);
+  return r;
 }
 
 //
@@ -115,17 +115,17 @@ uint8_t OneWire::reset() {
 // more certain timing.
 //
 void OneWire::write_bit(uint8_t v) {
-    static uint8_t lowTime[] = { 55, 5 };
-    static uint8_t highTime[] = { 5, 55};
-    
-    v = (v&1);
-    *modeReg |= bitmask;  // make pin an output, do first since we
-                          // expect to be at 1
-    *outputReg &= ~bitmask; // zero
-    delayMicroseconds(lowTime[v]);
-    *outputReg |= bitmask; // one, push pin up - important for
-                           // parasites, they might start in here
-    delayMicroseconds(highTime[v]);
+  static uint8_t lowTime[] = { 55, 5 };
+  static uint8_t highTime[] = { 5, 55};
+
+  v = (v&1);
+  *modeReg |= bitmask;  // make pin an output, do first since we
+  // expect to be at 1
+  *outputReg &= ~bitmask; // zero
+  delayMicroseconds(lowTime[v]);
+  *outputReg |= bitmask; // one, push pin up - important for
+                         // parasites, they might start in here
+  delayMicroseconds(highTime[v]);
 }
 
 //
@@ -133,17 +133,17 @@ void OneWire::write_bit(uint8_t v) {
 // more certain timing.
 //
 uint8_t OneWire::read_bit() {
-    uint8_t r;
-    
-    *modeReg |= bitmask;    // make pin an output, do first since we expect to be at 1
-    *outputReg &= ~bitmask; // zero
-    delayMicroseconds(1);
-    *modeReg &= ~bitmask;     // let pin float, pull up will raise
-    delayMicroseconds(5);          // A "read slot" is when 1mcs > t > 2mcs
-    r = ( *inputReg & bitmask) ? 1 : 0; // check the bit
-    delayMicroseconds(50);        // whole bit slot is 60-120uS, need to give some time
-    
-    return r;
+  uint8_t r;
+
+  *modeReg |= bitmask;    // make pin an output, do first since we expect to be at 1
+  *outputReg &= ~bitmask; // zero
+  delayMicroseconds(1);
+  *modeReg &= ~bitmask;     // let pin float, pull up will raise
+  delayMicroseconds(5);          // A "read slot" is when 1mcs > t > 2mcs
+  r = (*inputReg & bitmask) ? 1 : 0; // check the bit
+  delayMicroseconds(50);        // whole bit slot is 60-120uS, need to give some time
+
+  return r;
 }
 
 //
@@ -154,40 +154,42 @@ uint8_t OneWire::read_bit() {
 // other mishap.
 //
 void OneWire::write(uint8_t v, uint8_t power) {
-    uint8_t bitMask;
-    
-    for (bitMask = 0x01; bitMask; bitMask <<= 1) {
-	OneWire::write_bit( (bitMask & v)?1:0);
-    }
-    if ( !power) {
-	pinMode(pin,INPUT);
-	digitalWrite(pin,0);
-    }
+  uint8_t bitMask;
+
+  for (bitMask = 0x01; bitMask; bitMask <<= 1) {
+    OneWire::write_bit((bitMask & v) ? 1 : 0);
+  }
+  if (!power) {
+    pinMode(pin,INPUT);
+    digitalWrite(pin,0);
+  }
 }
 
 //
 // Read a byte
 //
 uint8_t OneWire::read() {
-    uint8_t bitMask;
-    uint8_t r = 0;
-    
-    for (bitMask = 0x01; bitMask; bitMask <<= 1) {
-	if ( OneWire::read_bit()) r |= bitMask;
+  uint8_t bitMask;
+  uint8_t r = 0;
+
+  for (bitMask = 0x01; bitMask; bitMask <<= 1) {
+    if (OneWire::read_bit()) {
+      r |= bitMask;
     }
-    return r;
+  }
+  return r;
 }
 
 //
 // Do a ROM select
 //
-void OneWire::select( uint8_t rom[8])
+void OneWire::select(uint8_t rom[8])
 {
-    int i;
-
-    write(0x55,0);         // Choose ROM
-
-    for( i = 0; i < 8; i++) write(rom[i],0);
+  int i;
+  write(0x55,0);         // Choose ROM
+  for(i = 0; i < 8; i++) {
+    write(rom[i],0);
+  }
 }
 
 //
@@ -195,12 +197,12 @@ void OneWire::select( uint8_t rom[8])
 //
 void OneWire::skip()
 {
-    write(0xCC,0);         // Skip ROM
+  write(0xCC, 0);         // Skip ROM
 }
 
 void OneWire::depower()
 {
-    pinMode(pin,INPUT);
+  pinMode(pin, INPUT);
 }
 
 #if ONEWIRE_SEARCH
@@ -211,14 +213,16 @@ void OneWire::depower()
 //
 void OneWire::reset_search()
 {
-    uint8_t i;
-    
-    searchJunction = -1;
-    searchExhausted = 0;
-    for( i = 7; ; i--) {
-	address[i] = 0;
-	if ( i == 0) break;
+  uint8_t i;
+
+  searchJunction = -1;
+  searchExhausted = 0;
+  for(i = 7; ; i--) {
+    address[i] = 0;
+    if (i == 0) {
+      break;
     }
+  }
 }
 
 //
@@ -311,22 +315,23 @@ uint8_t OneWire::search(uint8_t *newAddr)
 // This table comes from Dallas sample code where it is freely reusable, 
 // though Copyright (C) 2000 Dallas Semiconductor Corporation
 static uint8_t dscrc_table[] = {
-      0, 94,188,226, 97, 63,221,131,194,156,126, 32,163,253, 31, 65,
-    157,195, 33,127,252,162, 64, 30, 95,  1,227,189, 62, 96,130,220,
-     35,125,159,193, 66, 28,254,160,225,191, 93,  3,128,222, 60, 98,
-    190,224,  2, 92,223,129, 99, 61,124, 34,192,158, 29, 67,161,255,
-     70, 24,250,164, 39,121,155,197,132,218, 56,102,229,187, 89,  7,
-    219,133,103, 57,186,228,  6, 88, 25, 71,165,251,120, 38,196,154,
-    101, 59,217,135,  4, 90,184,230,167,249, 27, 69,198,152,122, 36,
-    248,166, 68, 26,153,199, 37,123, 58,100,134,216, 91,  5,231,185,
-    140,210, 48,110,237,179, 81, 15, 78, 16,242,172, 47,113,147,205,
-     17, 79,173,243,112, 46,204,146,211,141,111, 49,178,236, 14, 80,
-    175,241, 19, 77,206,144,114, 44,109, 51,209,143, 12, 82,176,238,
-     50,108,142,208, 83, 13,239,177,240,174, 76, 18,145,207, 45,115,
-    202,148,118, 40,171,245, 23, 73,  8, 86,180,234,105, 55,213,139,
-     87,  9,235,181, 54,104,138,212,149,203, 41,119,244,170, 72, 22,
-    233,183, 85, 11,136,214, 52,106, 43,117,151,201, 74, 20,246,168,
-    116, 42,200,150, 21, 75,169,247,182,232, 10, 84,215,137,107, 53};
+    0, 94,188,226, 97, 63,221,131,194,156,126, 32,163,253, 31, 65,
+  157,195, 33,127,252,162, 64, 30, 95,  1,227,189, 62, 96,130,220,
+   35,125,159,193, 66, 28,254,160,225,191, 93,  3,128,222, 60, 98,
+  190,224,  2, 92,223,129, 99, 61,124, 34,192,158, 29, 67,161,255,
+   70, 24,250,164, 39,121,155,197,132,218, 56,102,229,187, 89,  7,
+  219,133,103, 57,186,228,  6, 88, 25, 71,165,251,120, 38,196,154,
+  101, 59,217,135,  4, 90,184,230,167,249, 27, 69,198,152,122, 36,
+  248,166, 68, 26,153,199, 37,123, 58,100,134,216, 91,  5,231,185,
+  140,210, 48,110,237,179, 81, 15, 78, 16,242,172, 47,113,147,205,
+   17, 79,173,243,112, 46,204,146,211,141,111, 49,178,236, 14, 80,
+  175,241, 19, 77,206,144,114, 44,109, 51,209,143, 12, 82,176,238,
+   50,108,142,208, 83, 13,239,177,240,174, 76, 18,145,207, 45,115,
+  202,148,118, 40,171,245, 23, 73,  8, 86,180,234,105, 55,213,139,
+   87,  9,235,181, 54,104,138,212,149,203, 41,119,244,170, 72, 22,
+  233,183, 85, 11,136,214, 52,106, 43,117,151,201, 74, 20,246,168,
+  116, 42,200,150, 21, 75,169,247,182,232, 10, 84,215,137,107, 53
+};
 
 //
 // Compute a Dallas Semiconductor 8 bit CRC. These show up in the ROM
@@ -335,13 +340,13 @@ static uint8_t dscrc_table[] = {
 // compared to all those delayMicrosecond() calls.  But I got
 // confused, so I use this table from the examples.)  
 //
-uint8_t OneWire::crc8( uint8_t *addr, uint8_t len)
+uint8_t OneWire::crc8(uint8_t *addr, uint8_t len)
 {
   uint8_t i;
   uint8_t crc = 0;
 
   for (i = 0; i < len; i++) {
-    crc  = dscrc_table[ crc ^ addr[i] ];
+    crc = dscrc_table[crc ^ addr[i]];
   }
   return crc;
 }
@@ -376,23 +381,23 @@ static short oddparity[16] = { 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0 };
 //
 unsigned short OneWire::crc16(unsigned short *data, unsigned short len)
 {
-    unsigned short i;
-    unsigned short crc = 0;
-    
-    for ( i = 0; i < len; i++) {
-	unsigned short cdata = data[len];
-	
-	cdata = (cdata ^ (crc & 0xff)) & 0xff;
-	crc >>= 8;
-	
-	if (oddparity[cdata & 0xf] ^ oddparity[cdata >> 4]) crc ^= 0xc001;
-	
-	cdata <<= 6;
-	crc ^= cdata;
-	cdata <<= 1;
-	crc ^= cdata;
-    }
-    return crc;
+  unsigned short i;
+  unsigned short crc = 0;
+
+  for (i = 0; i < len; i++) {
+    unsigned short cdata = data[len];
+
+    cdata = (cdata ^ (crc & 0xff)) & 0xff;
+    crc >>= 8;
+
+    if (oddparity[cdata & 0xf] ^ oddparity[cdata >> 4]) crc ^= 0xc001;
+
+    cdata <<= 6;
+    crc ^= cdata;
+    cdata <<= 1;
+    crc ^= cdata;
+  }
+  return crc;
 }
 #endif
 

c0e2a67294159924eebf8073f8e65250764eab79
mike <opensource@hoho.com>
2010-04-13 23:04:19 -0700

    Simplify test running.

--- pykeg/unittest_settings.py
+++ pykeg/unittest_settings.py
@@ -1,4 +1,4 @@
-# Django settings module for use with unittests
+from settings import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -7,56 +7,16 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = 'unittest_db.sqlite.bin'
 TIME_ZONE = 'America/Los_Angeles'
-
-# Language code for this installation. All choices can be found here:
-# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-# http://blogs.law.harvard.edu/tech/stories/storyReader$15
 LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
-
 MEDIA_ROOT = ''
 MEDIA_URL = ''
 ADMIN_MEDIA_PREFIX = '/media/'
 SECRET_KEY = ''
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.doc.XViewMiddleware',
-)
-
-ROOT_URLCONF = 'pykeg.urls'
-
-TEMPLATE_DIRS = (
-    "templates",
-)
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.admin',
-    'django.contrib.humanize',
-    'pykeg.core',
-    'pykeg.contrib.twitter',
-    'pykeg.contrib.soundserver',
-    'pykeg.billing',
-    'kegweb.kegweb',
-)
-
-AUTH_PROFILE_MODULE = "core.UserProfile"
 
 # add external paths
 import os
 import sys
 _local_dir = os.path.dirname(sys.modules[__name__].__file__)
 _package_dir = os.path.normpath(os.path.join(_local_dir, '..'))
-sys.path.append(os.path.join(_package_dir, 'pykeg', 'external'))
+sys.path.insert(0, os.path.join(_package_dir, 'pykeg', 'external'))

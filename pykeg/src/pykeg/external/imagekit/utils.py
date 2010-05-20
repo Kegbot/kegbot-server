0490780f696cad04875c4e1b5cb210fb401aa040
mike <opensource@hoho.com>
2010-05-19 22:09:01 -0700

    Remove django-imagekit mirror (avilable in pypi).

--- pykeg/src/pykeg/external/imagekit/utils.py
+++
@@ -1,9 +0,0 @@
-""" ImageKit utility functions """
-
-import tempfile
-
-def img_to_fobj(img, format, **kwargs):
-    tmp = tempfile.TemporaryFile()
-    img.save(tmp, format, **kwargs)
-    tmp.seek(0)
-    return tmp

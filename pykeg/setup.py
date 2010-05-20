fa448fc0cbb3b483bd5a052d2146dcda6d8c2f86
mike <opensource@hoho.com>
2010-05-19 21:29:24 -0700

    Update setup.py with progress thus far.

--- pykeg/setup.py
+++ pykeg/setup.py
@@ -1,31 +1,57 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
 
 setup(
-    name = "pykeg",
-    version = "1.9.0",
+    name = "kegbot",
+    version = "0.6.0",
     description = "Kegbot keg controller software",
     author = "mike wakerly",
-    author_email = "mike@kegbot.org",
+    author_email = "opensource@hoho.com",
     url = "http://kegbot.org/",
-    packages = [
-      'pykeg',
-      'pykeg.core',
-      'pykeg.templates',
-    ],
-    package_dir = {
-      'pykeg': '',
-    },
+    packages = find_packages('src', exclude=[
+        "*.tests",
+        "*.tests.*",
+        "*south*",
+        "*imagekit*",
+        "*gflags*",
+    ]),
     package_data = {
       'pykeg' : ['settings.py'],
       'pykeg.templates' : ['*.html', '*/*.html'],
     },
+    package_dir = {
+      '' : 'src',
+    },
     data_files = [
-      ('/etc/pykeg', ['settings.py']),
+      ('/etc/pykeg', [
+          'common_settings.py.example',
+          'kegbot_master.cfg.example',
+      ]),
     ],
     scripts = [
-      'bin/kb_core',
-      'bin/kb_webserver',
+      'src/pykeg/bin/fb_publisher.py',
+      'src/pykeg/bin/kegboard_daemon.py',
+      'src/pykeg/bin/kegboard_monitor.py',
+      'src/pykeg/bin/kegbot_admin.py',
+      'src/pykeg/bin/kegbot_core.py',
+      'src/pykeg/bin/kegbot_master.py',
+      'src/pykeg/bin/lcd_daemon.py',
+      'src/pykeg/bin/rfid_daemon.py',
+      'src/pykeg/bin/sound_server.py',
     ],
+    install_requires = [
+      'distribute',
+      'django >= 1.0',
+      'django-imagekit >= 0.3.3',
+      'pylcdui >= 0.5.4',
+      'python-gflags >= 1.3',
+      'South >= 0.7',
+      #'MySQLdb>=1.2.1p2',
+      #'pysqlite>=2.0.3',
+    ],
+
 )

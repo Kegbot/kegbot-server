fa428d3e35d2b6aeb6c176f5d97d449d3b310114
mike <opensource@hoho.com>
2010-11-28 19:07:33 -0800

    Update to v0.7.6

--- pykeg/setup.py
+++ pykeg/setup.py
@@ -5,7 +5,7 @@ use_setuptools()
 
 from setuptools import setup, find_packages
 
-VERSION = "0.7.5"
+VERSION = "0.7.6"
 SHORT_DESCRIPTION = "Kegbot kegerator controller software"
 LONG_DESCRIPTION = """This package contains Kegbot core controller and Django
 frontend package.

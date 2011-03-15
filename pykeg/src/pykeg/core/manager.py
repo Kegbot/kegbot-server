e58eb4163659b28ab1f536f6bd267088ddce04d8
mike <opensource@hoho.com>
2011-03-14 22:53:12 -0700

    core: fix error due to id string change.

--- pykeg/src/pykeg/core/manager.py
+++ pykeg/src/pykeg/core/manager.py
@@ -466,7 +466,7 @@ class DrinkManager(Manager):
     keg_id = d.get('keg_id', None)
     username = d.get('user_id', '<None>')
 
-    self._logger.info('Logged drink %i username=%s keg=%s liters=%.2f ticks=%i' % (
+    self._logger.info('Logged drink %s username=%s keg=%s liters=%.2f ticks=%i' % (
       d.id, username, keg_id, d.volume_ml/1000.0, d.ticks))
 
     self._last_drink = d

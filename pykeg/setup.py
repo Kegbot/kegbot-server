43c96e89f74525cccbeb97604b1c82252a7483dd
Shane Farmer <shane.farmer@gmail.com>
2012-05-23 23:09:12 -0700

    Added files required for AppEngine and started on a Paver setup script
    * Added AppEngine app.yaml and some support files.
    * Created a paver file that should be able to create a virtualenv for getting required modules.
    * Modified setup.py so it can be included as a module

--- pykeg/setup.py
+++ pykeg/setup.py
@@ -1,104 +1,122 @@
 #!/usr/bin/env python
+"""Kegbot kegerator controller software
 
-from distribute_setup import use_setuptools
-use_setuptools()
-
-from setuptools import setup, find_packages
-
-VERSION = '0.9.0'
-SHORT_DESCRIPTION = 'Kegbot kegerator controller software'
-LONG_DESCRIPTION = """This package contains Kegbot core controller and Django
+This package contains Kegbot core controller and Django
 frontend package.
 
 Kegbot is a hardware and software system to record and monitor access to a beer
 kegerator.  For more information and documentation, see http://kegbot.org/
 """
 
-setup(
-    name = 'kegbot',
-    version = VERSION,
-    description = SHORT_DESCRIPTION,
-    long_description = LONG_DESCRIPTION,
-    author = 'mike wakerly',
-    author_email = 'opensource@hoho.com',
-    url = 'http://kegbot.org/',
-    packages = find_packages('src'),
-    package_dir = {
-      '' : 'src',
-    },
-    scripts = [
-      'distribute_setup.py',
-      'src/pykeg/bin/kegboard_daemon.py',
-      'src/pykeg/bin/kegboard_monitor.py',
-      'src/pykeg/bin/kegboard-tester.py',
-      'src/pykeg/bin/kegbot-admin.py',
-      'src/pykeg/bin/kegbot_core.py',
-      'src/pykeg/bin/kegbot_master.py',
-      'src/pykeg/bin/lcd_daemon.py',
-      'src/pykeg/bin/rfid_daemon.py',
-      'src/pykeg/bin/sound_server.py',
-    ],
-    install_requires = [
-      'django >= 1.3',
-      'django-autoslug',
-      'django-bootstrap-form',
-      'django-imagekit >= 2.0',
-      'django-registration',
-      'django-socialregistration >= 0.5.4',
-      'django_extensions',
+DOCLINES = __doc__.split('\n')
 
-      'facebook-sdk >= 0.3.0',
+# Change this to True to include optional dependencies
+USE_OPTIONAL = False
 
-      'MySQL-python',
-      'pil',
-      'protobuf >= 2.4.1',
-      'pylcdui >= 0.5.5',
-      'pysqlite>=2.0.3',
-      'python-gflags >= 1.8',
-      'South >= 0.7.3',
-      'Sphinx',
-      'django_nose',
-      'tweepy',
-      'django-icanhaz',
-      'pytz',
+VERSION = '0.9.0'
+SHORT_DESCRIPTION = DOCLINES[0]
+LONG_DESCRIPTION = '\n'.join(DOCLINES[2:])
+REQUIRED = [
+  'django >= 1.3',
+  'django-autoslug',
+  'django-bootstrap-form',
+  'django-imagekit >= 2.0',
+  'django-registration',
+  'django-socialregistration >= 0.5.4',
+  'django_extensions',
 
-      'requests',  # needed by oauth
-      'poster',  # needed by foursquare
-      'foursquare',
+  'facebook-sdk >= 0.3.0',
 
-      ### Optional modules.
+  'MySQL-python',
+  'pil',
+  'protobuf >= 2.4.1',
+  'pylcdui >= 0.5.5',
+  'pysqlite>=2.0.3',
+  'python-gflags >= 1.8',
+  'South >= 0.7.3',
+  'Sphinx',
+  'django_nose',
+  'tweepy',
+  'django-icanhaz',
+  'pytz',
 
-      # This modules 'pip install'd manually.  If you add to this list, also
-      # update pykeg.core.optional_modules with a new 'HAVE_<MODULE>' flag, as
-      # well as pykeg.settings (if needed).
+  'requests', # needed by oauth
+  'poster', # needed by foursquare
+  'foursquare',
+]
 
-      ### django-debug-toolbar
-      #'django-debug-toolbar',
+OPTIONAL = [
+  ### Optional modules.
+  # This modules 'pip install'd manually.  If you add to this list, also
+  # update pykeg.core.optional_modules with a new 'HAVE_<MODULE>' flag, as
+  # well as pykeg.settings (if needed).
 
-      ### Celery
-      #'Celery',
-      #'django-celery',
-      #'django-kombu',
+  ### django-debug-toolbar
+  'django-debug-toolbar',
 
-      ### Tornado
-      #'tornado',
-      #'rjdj.djangotornado',
+  ### Celery
+  'Celery',
+  'django-celery',
+  'django-kombu',
 
-      ### Raven
-      #'raven',
+  ### Tornado
+  'tornado',
+  'rjdj.djangotornado',
 
-      ### Sentry
-      #'sentry',
-      #'django-sentry',
-    ],
-    dependency_links = [
-        'https://github.com/rem/python-protobuf/tarball/master#egg=protobuf-2.4.1',
-        'https://github.com/tzangms/django-bootstrap-form/tarball/master#egg=django-bootstrap-form',
-        'https://github.com/rjdj/django-tornado/tarball/master#egg=rjdj.djangotornado',
-    ],
-    include_package_data = True,
-    entry_points = {
-      'console_scripts': ['instance=django.core.management:execute_manager'],
-    },
+  ### Raven
+  'raven',
 
-)
+  ### Sentry
+  'sentry',
+  'django-sentry',
+]
+
+DEPENDENCIES = REQUIRED
+if USE_OPTIONAL:
+  DEPENDENCIES += OPTIONAL
+
+
+def setup_package():
+  from distribute_setup import use_setuptools
+  use_setuptools()
+  from setuptools import setup, find_packages
+
+  setup(
+      name = 'kegbot',
+      version = VERSION,
+      description = SHORT_DESCRIPTION,
+      long_description = LONG_DESCRIPTION,
+      author = 'mike wakerly',
+      author_email = 'opensource@hoho.com',
+      url = 'http://kegbot.org/',
+      packages = find_packages('src'),
+      package_dir = {
+        '' : 'src',
+      },
+      scripts = [
+        'distribute_setup.py',
+        'src/pykeg/bin/kegboard_daemon.py',
+        'src/pykeg/bin/kegboard_monitor.py',
+        'src/pykeg/bin/kegboard-tester.py',
+        'src/pykeg/bin/kegbot-admin.py',
+        'src/pykeg/bin/kegbot_core.py',
+        'src/pykeg/bin/kegbot_master.py',
+        'src/pykeg/bin/lcd_daemon.py',
+        'src/pykeg/bin/rfid_daemon.py',
+        'src/pykeg/bin/sound_server.py',
+      ],
+      install_requires = DEPENDENCIES,
+      dependency_links = [
+          'https://github.com/rem/python-protobuf/tarball/master#egg=protobuf-2.4.1',
+          'https://github.com/tzangms/django-bootstrap-form/tarball/master#egg=django-bootstrap-form',
+          'https://github.com/rjdj/django-tornado/tarball/master#egg=rjdj.djangotornado',
+      ],
+      include_package_data = True,
+      entry_points = {
+        'console_scripts': ['instance=django.core.management:execute_manager'],
+      },
+
+  )
+
+if __name__ == '__main__':
+  setup_package()

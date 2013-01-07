418587ea9a094aa5f50a21536b8cf45a74b01782
mike wakerly <opensource@hoho.com>
2013-01-06 19:21:11 -0800

    context processor: add DEBUG to context

--- pykeg/web/context_processors.py
+++ pykeg/web/context_processors.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from pykeg.core import features
 from pykeg.core import models
 from pykeg.web.kegweb.forms import LoginForm
@@ -26,6 +28,7 @@ def kbsite(request):
     guest_info['image'] = kbsite.settings.guest_image
 
   ret = {
+    'DEBUG': settings.DEBUG,
     'kbsite': getattr(request, 'kbsite', None),
     'request_path': request.path,
     'login_form': LoginForm(initial={'next_page': request.path}),

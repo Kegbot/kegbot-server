f9d2be30ff2345ca69b24ce4f5fe4a0e644ee612
Colin Edwards <colin@recursivepenguin.com>
2013-08-17 16:30:37 -0500

    Inital work on sending emails when kegs are 10% full

--- pykeg/web/kegadmin/forms.py
+++ pykeg/web/kegadmin/forms.py
@@ -134,6 +134,7 @@ class SiteSettingsForm(forms.ModelForm):
             'registration_allowed',
             'registration_confirmation',
             'allowed_hosts',
+            'keg_empty_notification_email_address'
         )
 
     helper = FormHelper()
@@ -156,6 +157,7 @@ class SiteSettingsForm(forms.ModelForm):
         Field('registration_allowed', css_class='input-xlarge'),
         Field('registration_confirmation', css_class='input-xlarge'),
         Field('allowed_hosts'),
+        Field('keg_empty_notification_email_address'),
         FormActions(
             Submit('submit', 'Save Settings', css_class='btn-primary'),
         )

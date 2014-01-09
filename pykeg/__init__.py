ffc5ce5fc74d28e6f72da0454785200d3070957a
mike wakerly <opensource@hoho.com>
2014-01-09 13:51:12 -0900

    Do not create shared celery instance unless setup.

--- pykeg/__init__.py
+++ pykeg/__init__.py
@@ -3,7 +3,16 @@
 # Internal version number, bumped every time "kb_upgrade" is needed.
 EPOCH = 103
 
+try:
+    import local_settings
+    IS_SETUP = True
+except ImportError:
+    IS_SETUP = False
+
 ### Celery setup
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+# HACK: Avoid bootstrapping Celery until setup has been run,
+# since it depends on settings -> local_settings.
+if IS_SETUP:
+    from .celery import app as celery_app

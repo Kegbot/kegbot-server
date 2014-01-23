dd71763e312376f4d2a5836bf3a4bf55432286b7
mike wakerly <opensource@hoho.com>
2014-01-22 18:19:18 -0800

    refactor: Custom User model, part 2.
    Add pykeg.core.models.User, a copy of auth.User.

--- pykeg/core/models.py
+++ pykeg/core/models.py
@@ -21,6 +21,7 @@ import datetime
 import os
 import pytz
 import random
+import re
 
 from django.core.urlresolvers import reverse
 from django.db import models
@@ -28,7 +29,14 @@ from django.db import IntegrityError
 from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
 from django.contrib.auth.models import User as AuthUser
+import django.contrib.auth.models as auth_models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import UserManager
+from django.utils.translation import ugettext_lazy as _
+from django.core import validators
+from django.contrib import auth
+from django.core.mail import send_mail
 
 from imagekit.models import ImageSpecField
 from imagekit.processors import Adjust
@@ -49,6 +57,132 @@ from kegbot.util import util
 """Django models definition for the kegbot database."""
 
 TIMEZONE_CHOICES = ((z, z) for z in pytz.common_timezones)
+
+
+class User(AbstractBaseUser):
+    # The reverse names of related fields in PermissionsMixin cause a
+    # conflict if we just extend AbstractUser.  Workaround: compose
+    # User "by hand", modifying related_name where necessary.
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    ### PermissionsMixin
+    is_superuser = models.BooleanField(_('superuser status'), default=False,
+        help_text=_('Designates that this user has all permissions without '
+                    'explicitly assigning them.'))
+    groups = models.ManyToManyField('auth.Group', verbose_name=_('groups'),
+        blank=True, help_text=_('The groups this user belongs to. A user will '
+                                'get all permissions granted to each of '
+                                'his/her group.'),
+        related_name="user_set_2", related_query_name="user")
+    user_permissions = models.ManyToManyField('auth.Permission',
+        verbose_name=_('user permissions'), blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name="user_set_2", related_query_name="user")
+
+    ### AbstractUser
+
+    username = models.CharField(_('username'), max_length=30, unique=True,
+        help_text=_('Required. 30 characters or fewer. Letters, numbers and '
+                    '@/./+/-/_ characters'),
+        validators=[
+            validators.RegexValidator(re.compile('^[\w.@+-]+$'), _('Enter a valid username.'), 'invalid')
+        ])
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), blank=True)
+    is_staff = models.BooleanField(_('staff status'), default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    ### AbstractUser methods
+
+    def get_absolute_url(self):
+        return "/users/%s/" % urlquote(self.username)
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        "Returns the short name for the user."
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email])
+
+    def get_profile(self):
+        raise NotImplemented
+
+    ### PermissionsMixin methods
+
+    def get_group_permissions(self, obj=None):
+        """
+        Returns a list of permission strings that this user has through his/her
+        groups. This method queries all available auth backends. If an object
+        is passed in, only permissions matching this object are returned.
+        """
+        permissions = set()
+        for backend in auth.get_backends():
+            if hasattr(backend, "get_group_permissions"):
+                permissions.update(backend.get_group_permissions(self, obj))
+        return permissions
+
+    def get_all_permissions(self, obj=None):
+        return auth_models._user_get_all_permissions(self, obj)
+
+    def has_perm(self, perm, obj=None):
+        """
+        Returns True if the user has the specified permission. This method
+        queries all available auth backends, but returns immediately if any
+        backend returns True. Thus, a user who has permission from a single
+        auth backend is assumed to have permission in general. If an object is
+        provided, permissions for this specific object are checked.
+        """
+
+        # Active superusers have all permissions.
+        if self.is_active and self.is_superuser:
+            return True
+
+        # Otherwise we need to check the backends.
+        return auth_models._user_has_perm(self, perm, obj)
+
+    def has_perms(self, perm_list, obj=None):
+        """
+        Returns True if the user has each of the specified permissions. If
+        object is passed, it checks if the user has all required perms for this
+        object.
+        """
+        for perm in perm_list:
+            if not self.has_perm(perm, obj):
+                return False
+        return True
+
+    def has_module_perms(self, app_label):
+        """
+        Returns True if the user has any permissions in the given app label.
+        Uses pretty much the same logic as has_perm, above.
+        """
+        # Active superusers have all permissions.
+        if self.is_active and self.is_superuser:
+            return True
+
+        return auth_models._user_has_module_perms(self, app_label)
+
 
 class KegbotSite(models.Model):
     name = models.CharField(max_length=64, unique=True, default='default',
@@ -219,6 +353,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(AuthUser)
     mugshot = models.ForeignKey('Picture', blank=True, null=True,
       on_delete=models.SET_NULL)
+
 
 def _user_post_save(sender, instance, **kwargs):
     # HACK: Do not use get_or_create due to transaction.atomic conflict

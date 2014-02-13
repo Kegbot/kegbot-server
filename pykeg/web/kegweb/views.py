8064866188533d6754e744ed2a42e991a7938ddc
theguardian <justin.evans@gmail.com>
2014-02-12 16:59:00 -0800

    Created v1 Fullscreen Taplist

--- pykeg/web/kegweb/views.py
+++ pykeg/web/kegweb/views.py
@@ -102,6 +102,14 @@ class KegListView(ListView):
     def get_queryset(self):
         return models.Keg.objects.all().order_by('-id')
 
+class TaplistView(ListView):
+    model = models.Keg
+    template_name = 'kegweb/taplist.html'
+    context_object_name = 'kegs'
+
+    def get_queryset(self):
+        return models.Keg.objects.all().order_by('-id')
+
 @cache_page(30)
 def keg_detail(request, keg_id):
     keg = get_object_or_404(models.Keg, id=keg_id)

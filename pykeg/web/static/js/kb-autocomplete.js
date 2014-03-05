8769f2b83713d740675d0a7e58a2e34b68939ff0
Jared Szechy <jared.szechy@gmail.com>
2014-03-04 21:28:04 -0500

    Upgrade templates and stylesheets for bootstrap 3
    Bootstrap 3 updates
    
     *Added responsive class to images
     *Fixed image galleries
     *Fixed sub-navs
     *Added active to initial tabs
     *Fixed snapshots
    
    Bootstrap 3 responsive updates
    
     * Updated badges to make use of responsive col for sizing
     * Changed several columns to sm for better responsive appearance
     * Change to HTML5
     * Add viewport meta data for mobile
     * Fixed up recent activity layout
    
    Remove line-height from kb-badge-small
    
    Fix vertical alignment of badges
    
    More bootstrap 3 fixes
    
     * typeahead was removed from bootstrap 3
       * added current bundled version of typeahead
       * added required hanlebars.js
       * added bootstrap 3 version of typeahead.less
       * use bloodhound implementation for autocomplete
       * modified autocomplete api to include search tokens
     * fixed user status superuser label
     * fixed inline forms
    
    Fix bootstrap 3 error alerts
    
    alert-error is not part of bootstrap 3. create alert-error class
    that inherits from alert-danger.

---
+++ pykeg/web/static/js/kb-autocomplete.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+  var beverages = new Bloodhound({
+    datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.tokens.join(' ')); },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: beverage_complete_url + "?q=%QUERY"
+  });
+  beverages.initialize();
+
+  var beverage_labels, beverage_mapped;
+  $('.beer-select').each(function() {
+    var beverage_name = $(this).find('input[name=beverage_name]');
+    var beverage_id = $(this).find('input[name=beverage_id]');
+    var producer_name = $(this).find('input[name=producer_name]');
+    var producer_id = $(this).find('input[name=producer_id]');
+    var style = $(this).find('input[name=style_name]');
+
+    beverage_name.typeahead(null, {
+      name: 'beverage',
+      source: beverages.ttAdapter(),
+      displayKey: 'name',
+      templates: {
+        suggestion: Handlebars.compile(
+          '<p><strong>{{name}}</strong> ({{producer_name}})'
+        )
+      }
+    });
+
+    beverage_name.bind('typeahead:selected', function(obj, datum, name) {
+      producer_name.val(datum.producer_name);
+      producer_id.val(datum.producer_id);
+      style.val(datum.style);
+      beverage_id.val(datum.id);
+    });
+  });
+
+  var users = new Bloodhound({
+    datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.tokens.join(' ')); },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: user_complete_url + "?q=%QUERY"
+  });
+  users.initialize();
+
+  $('.user-select').each(function() {
+    var username = $(this).find('input[name=username]');
+    username.typeahead(null, {
+      name: 'user',
+      displayKey: 'username',
+      source: users.ttAdapter(),
+      templates: {
+        suggestion: Handlebars.compile(
+          '<p><strong>{{username}}</strong> ({{email}})</p>'
+        )
+      }
+    });
+  });
+});

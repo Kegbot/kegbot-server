f6d8200b68cf8d41cfe2ae20370da79e92542d8c
mike wakerly <opensource@hoho.com>
2014-04-10 18:57:05 -0700

    Embed SSO urls when available.

--- pykeg/web/context_processors.py
+++ pykeg/web/context_processors.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+import urllib
+
 import pykeg
 from pykeg.core import models
 from pykeg.core import util
@@ -7,6 +9,16 @@ from pykeg.web.kegweb.forms import LoginForm
 
 def kbsite(request):
     kbsite = getattr(request, 'kbsite', None)
+
+    redir = urllib.urlencode({'redir': request.build_absolute_uri(request.path)})
+
+    sso_login_url = getattr(settings, 'SSO_LOGIN_URL', '')
+    if sso_login_url:
+      sso_login_url = '{}?{}'.format(sso_login_url, redir)
+
+    sso_logout_url = getattr(settings, 'SSO_LOGOUT_URL', '')
+    if sso_logout_url:
+      sso_logout_url = '{}?{}'.format(sso_logout_url, redir)
 
     ret = {
       'DEBUG': settings.DEBUG,
@@ -17,6 +29,8 @@ def kbsite(request):
       'HAVE_SESSIONS': False,
       'HAVE_ADMIN': settings.KEGBOT_ENABLE_ADMIN,
       'GOOGLE_ANALYTICS_ID': None,
+      'SSO_LOGIN_URL': sso_login_url,
+      'SSO_LOGOUT_URL': sso_logout_url,
       'kbsite': kbsite,
       'request_path': request.path,
       'login_form': LoginForm(initial={'next_page': request.path}),

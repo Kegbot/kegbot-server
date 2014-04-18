79d9795b30ee630bfbc5714bfee6f537fea9905a
mike wakerly <opensource@hoho.com>
2014-04-17 19:59:03 -0700

    run_workers: Detect app name.

--- pykeg/core/management/commands/run_workers.py
+++ pykeg/core/management/commands/run_workers.py
@@ -16,6 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Pykeg.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+from django.conf import settings
+
 from pykeg.core.management.commands.common import RunnerCommand
 
 
@@ -26,6 +29,7 @@ class Command(RunnerCommand):
     def get_commands(self, options):
         default_log = stats_log = ''
         logs_dir = options.get('logs_dir')
+
         if logs_dir:
             default_log = ' --logfile="{}"'.format(
                 os.path.join(logs_dir, 'celery_default.log'))
@@ -33,9 +37,14 @@ class Command(RunnerCommand):
                 os.path.join(logs_dir, 'celery_stats.log'))
         ret = []
 
+        # Pluck out the app_name.  Typically this is 'pykeg', but may be
+        # something else in embedded mode.
+        app_name = settings.SETTINGS_MODULE.split('.')[0]
+        base_cmd = 'celery -A {} worker -l info '.format(app_name)
+
         ret.append(('celery_default',
-            'celery -A pykeg worker -l info -Q default --hostname="default@%h"' + default_log))
+            base_cmd + '-Q default --hostname="default@%h"' + default_log))
         ret.append(('celery_stats',
-            'celery -A pykeg worker -l info -Q stats --concurrency=1 --hostname="stats@%h"' + stats_log))
+            base_cmd + '-Q stats --concurrency=1 --hostname="stats@%h"' + stats_log))
 
         return ret

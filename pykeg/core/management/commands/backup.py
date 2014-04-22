7b4691718d283f80d21e9f3816bb3178d303627b
mike wakerly <opensource@hoho.com>
2014-04-22 16:01:16 -0700

    backup: Print full file path when available.

--- pykeg/core/management/commands/backup.py
+++ pykeg/core/management/commands/backup.py
@@ -16,6 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Pykeg.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+
+from django.core.files.storage import get_storage_class
 from django.core.management.base import NoArgsCommand
 from pykeg.core import backup
 
@@ -24,4 +27,7 @@ class Command(NoArgsCommand):
 
     def handle(self, **options):
         location = backup.backup()
+        storage = get_storage_class()()
+        if hasattr(storage, 'location'):
+            location = os.path.join(storage.location, location)
         print 'Backup complete! Backup file:\n  {}'.format(location)

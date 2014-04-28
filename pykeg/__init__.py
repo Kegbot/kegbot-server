4cbfac8f3983f854e209d7bcbf732eaf3d197c18
mike wakerly <opensource@hoho.com>
2014-04-28 07:00:09 -0700

    Delete celery_app setup.

--- pykeg/__init__.py
+++ pykeg/__init__.py
@@ -2,17 +2,3 @@
 
 # Internal version number, bumped every time "kegbot upgrade" is needed.
 EPOCH = 106
-
-try:
-    import local_settings
-    IS_SETUP = True
-except ImportError:
-    IS_SETUP = False
-
-### Celery setup
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-# HACK: Avoid bootstrapping Celery until setup has been run,
-# since it depends on settings -> local_settings.
-if IS_SETUP:
-    from .celery import app as celery_app

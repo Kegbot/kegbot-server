895293582ad8ebc9d9733c3fbcc12cd862ddefc2
mike wakerly <opensource@hoho.com>
2014-05-28 09:53:46 -0700

    Add djcelery_email to installed_apps.
    Issue #283.

--- pykeg/settings.py
+++ pykeg/settings.py
@@ -373,6 +373,7 @@ if KEGBOT_ENABLE_ADMIN:
 
 if HAVE_CELERY_EMAIL:
     CELERY_EMAIL_BACKEND = EMAIL_BACKEND
+    INSTALLED_APPS += ('djcelery_email',)
     EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
 
 ### debug_toolbar

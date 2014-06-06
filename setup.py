b1a59981fc795b2b20015ca086a94721957d3c2c
mike wakerly <opensource@hoho.com>
2014-06-06 11:13:33 -0700

    v1.0.0a3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ kegerator.  For more information and documentation, see http://kegbot.org/
 
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0a2'
+VERSION = '1.0.0a3'
 DOCLINES = __doc__.split('\n')
 
 SHORT_DESCRIPTION = DOCLINES[0]

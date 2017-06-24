02d69874915a1fe32444cb7d35097e68f4a949e2
mike wakerly <opensource@hoho.com>
2017-06-24 15:10:50 -0400

    Django 1.10 compat: fix static/media serving.

--- pykeg/web/urls.py
+++ pykeg/web/urls.py
@@ -20,9 +20,9 @@ from __future__ import absolute_import
 
 from django.conf import settings
 from django.conf.urls import include
+from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.base import RedirectView
 
 admin.autodiscover()
@@ -50,10 +50,8 @@ if 'pykeg.web.setup_wizard' in settings.INSTALLED_APPS:
     ]
 
 if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += [
-      url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, }),
-    ]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.KEGBOT_ENABLE_ADMIN:
     urlpatterns += [

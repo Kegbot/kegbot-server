8703a792ab6124a17934fb519bedc2d5cb6d0c99
mike wakerly <opensource@hoho.com>
2017-06-24 11:03:14 -0400

    Update redis, django-redis.

--- pykeg/settings.py
+++ pykeg/settings.py
@@ -148,10 +148,10 @@ AUTHENTICATION_BACKENDS = (
 
 CACHES = {
     'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
+        'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': '127.0.0.1:6379:1',
         'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }

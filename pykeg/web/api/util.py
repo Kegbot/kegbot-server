241eff657633408c76867d48869cab61fc845665
mike wakerly <opensource@hoho.com>
2017-06-24 12:37:32 -0400

    Django 1.9 compat: `request.REQUEST` deprecated.

--- pykeg/web/api/util.py
+++ pykeg/web/api/util.py
@@ -59,7 +59,7 @@ def check_api_key(request):
     """Check a request for an API key."""
     keystr = request.META.get('HTTP_X_KEGBOT_API_KEY')
     if not keystr:
-        keystr = request.REQUEST.get('api_key')
+        keystr = request.POST.get('api_key', request.GET.get('api_key', None))
     if not keystr:
         raise kbapi.NoAuthTokenError('The parameter "api_key" is required')
 

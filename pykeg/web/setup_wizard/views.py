006f73e91c9711ff4a91805fd6eff040175feaf4
mike wakerly <opensource@hoho.com>
2020-04-18 12:51:10 -0400

    setup_wizard: perform database migrations when needed
    Should make the "first launch" case work, provided the
    database has been created and is reachable.
    
    Adds `/setup/upgrade` for subsequent situations where migration
    is needed.

--- pykeg/web/setup_wizard/views.py
+++ pykeg/web/setup_wizard/views.py
@@ -17,10 +17,12 @@
 # along with Pykeg.  If not, see <http://www.gnu.org/licenses/>.
 
 from functools import wraps
+import logging
+import traceback
 from django.conf import settings
+from django.core import management
 from django.contrib import messages
 from django.contrib.auth import authenticate
-from django.contrib.auth import login
 from django.http import Http404
 from django.shortcuts import redirect
 from django.shortcuts import render
@@ -28,9 +30,13 @@ from django.views.decorators.cache import never_cache
 
 from pykeg.core import defaults
 from pykeg.core import models
+from pykeg.util import dbstatus
+from pykeg.core.util import get_version_object
 
 from .forms import AdminUserForm
 from .forms import MiniSiteSettingsForm
+
+logger = logging.getLogger(__name__)
 
 
 def setup_view(f):
@@ -48,21 +54,79 @@ def setup_view(f):
 @setup_view
 @never_cache
 def start(request):
-    """ Shows the enable/disable hardware toggle. """
+    """Shows database setup button"""
+    context = {}
+
+    if request.method == 'POST':
+        try:
+            management.call_command("migrate", no_input=True)
+            return redirect('setup_mode')
+        except Exception as e:
+            logger.exception('Error installing database')
+            context['error_message'] = str(e)
+            context['error_stack'] = traceback.format_exc()
+    else:
+        try:
+            logger.info('Checking database status ...')
+            dbstatus.check_db_status()
+            logger.info('Database status OK.')
+        except dbstatus.DatabaseNotInitialized:
+            context['need_install'] = True
+        except dbstatus.NeedMigration:
+            context['need_upgrade'] = True
+
+    return render(request, 'setup_wizard/start.html', context=context)
+
+
+@setup_view
+@never_cache
+def mode(request):
+    """Shows the enable/disable hardware toggle."""
     context = {}
 
     if request.method == 'POST':
         if 'enable_sensing' in request.POST:
-            request.session['enable_sensing'] = True
-            return redirect('setup_accounts')
+            response = redirect('setup_accounts')
+            response.set_cookie('kb_setup_enable_sensing', 'True')
+            return response
         elif 'disable_sensing' in request.POST:
-            request.session['enable_sensing'] = False
-            request.session['enable_users'] = False
-            return redirect('setup_site_settings')
+            response = redirect('setup_site_settings')
+            response.set_cookie('kb_setup_enable_sensing', 'False')
+            response.set_cookie('kb_setup_enable_users', 'False')
+            return response
         else:
             messages.error(request, 'Unknown response.')
 
-    return render(request, 'setup_wizard/start.html', context=context)
+    return render(request, 'setup_wizard/mode.html', context=context)
+
+
+@setup_view
+@never_cache
+def upgrade(request):
+    context = {}
+    if request.method == 'POST':
+        try:
+            management.call_command("migrate", no_input=True)
+            site = models.KegbotSite.get()
+            app_version = get_version_object()
+            site.server_version = str(app_version)
+            site.save()
+            return redirect('kb-home')
+        except Exception as e:
+            logger.exception('Error installing database')
+            context['error_message'] = str(e)
+            context['error_stack'] = traceback.format_exc()
+
+    try:
+        logger.info('Checking database status ...')
+        dbstatus.check_db_status()
+        logger.info('Database status OK.')
+    except dbstatus.DatabaseNotInitialized:
+        context['message'] = 'Database not initialized'
+    except dbstatus.NeedMigration:
+        context['message'] = 'Database upgrade needed'
+
+    return render(request, 'setup_wizard/upgrade.html', context=context)
 
 
 @setup_view
@@ -73,11 +137,13 @@ def setup_accounts(request):
 
     if request.method == 'POST':
         if 'enable_users' in request.POST:
-            request.session['enable_users'] = True
-            return redirect('setup_site_settings')
+            response = redirect('setup_site_settings')
+            response.set_cookie('kb_setup_enable_users', 'True')
+            return response
         elif 'disable_users' in request.POST:
-            request.session['enable_users'] = False
-            return redirect('setup_site_settings')
+            response = redirect('setup_site_settings')
+            response.set_cookie('kb_setup_enable_users', 'False')
+            return response
         else:
             messages.error(request, 'Unknown response.')
 
@@ -90,7 +156,8 @@ def site_settings(request):
     context = {}
 
     if request.method == 'POST':
-        form = MiniSiteSettingsForm(request.POST, instance=request.kbsite)
+        site = models.KegbotSite.get()
+        form = MiniSiteSettingsForm(request.POST, instance=site)
         if form.is_valid():
             form.save()
             messages.success(request, 'Settings saved!')
@@ -102,8 +169,8 @@ def site_settings(request):
             pass
 
         site = models.KegbotSite.get()
-        site.enable_sensing = request.session['enable_sensing']
-        site.enable_users = request.session['enable_users']
+        site.enable_sensing = request.COOKIES.get('kb_setup_enable_sensing') == 'True'
+        site.enable_users = request.COOKIES.get('kb_setup_enable_users') == 'True'
         site.save()
         form = MiniSiteSettingsForm(instance=site)
     context['form'] = form
@@ -121,8 +188,6 @@ def admin(request):
             form.save()
             user = authenticate(username=form.cleaned_data.get('username'),
                                 password=form.cleaned_data.get('password'))
-            if user:
-                login(request, user)
             return redirect('setup_finish')
     context['form'] = form
     return render(request, 'setup_wizard/admin.html', context=context)
@@ -133,8 +198,9 @@ def admin(request):
 def finish(request):
     context = {}
     if request.method == 'POST':
-        request.kbsite.is_setup = True
-        request.kbsite.save()
+        site = models.KegbotSite.get()
+        site.is_setup = True
+        site.save()
         messages.success(request, 'Tip: Install a new Keg in Admin: Taps')
-        return redirect('kegadmin-main')
+        return redirect('kb-home')
     return render(request, 'setup_wizard/finish.html', context=context)

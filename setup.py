1d2ad18fbd96edb2eeebf35befac299c0df1840d
mike wakerly <opensource@hoho.com>
2020-04-18 15:41:56 -0400

    v1.3.0b2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ kegerator.  For more information and documentation, see http://kegbot.org/
 
 from setuptools import setup
 
-VERSION = '1.3.0b1'
+VERSION = '1.3.0b2'
 DOCLINES = __doc__.split('\n')
 
 SHORT_DESCRIPTION = DOCLINES[0]

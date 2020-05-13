6c62e939462764b7b16a9cd3fd3a6f666853beba
Johnnyruz <johnnyruzdev01@outlook.com>
2020-05-13 01:18:58 +0100

    Simplified backend logic

--- pykeg/backend/backends.py
+++ pykeg/backend/backends.py
@@ -659,15 +659,11 @@ class KegbotBackend(object):
     @transaction.atomic
     def connect_thermo(self, tap, new_thermo):
         tap = self._get_tap(tap)
-        old_thermo = tap.temperature_sensor
-
-        if old_thermo != new_thermo:
-            if old_thermo:
-                tap.temperature_sensor = None
-                tap.save()
-            if new_thermo:
-                tap.temperature_sensor = new_thermo
-                tap.save()
+        if new_thermo:
+            tap.temperature_sensor = new_thermo
+        else:
+            tap.temperature_sensor = None
+        tap.save()
 
         return tap
 

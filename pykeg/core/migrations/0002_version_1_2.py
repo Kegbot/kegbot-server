63a037e637a28e4ad929d70c3dce06d9992363e7
mike wakerly <opensource@hoho.com>
2022-07-15 13:48:02 +0100

    core: migrations: remove unnecessary binary strings (py3 change)
    `makemigrations` in python 3 would now cause a bunch of spurious migrations due
    strings, primarily `help_text`, appearing to change.

--- pykeg/core/migrations/0002_version_1_2.py
+++ pykeg/core/migrations/0002_version_1_2.py
@@ -45,21 +45,21 @@ class Migration(migrations.Migration):
             model_name="kegbotsite",
             name="enable_sensing",
             field=models.BooleanField(
-                default=True, help_text=b"Enable and show features related to volume sensing."
+                default=True, help_text="Enable and show features related to volume sensing."
             ),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name="kegbotsite",
             name="enable_users",
-            field=models.BooleanField(default=True, help_text=b"Enable user pour tracking."),
+            field=models.BooleanField(default=True, help_text="Enable user pour tracking."),
             preserve_default=True,
         ),
         migrations.AlterField(
             model_name="keg",
             name="description",
             field=models.TextField(
-                help_text=b"User-visible description of the Keg.", null=True, blank=True
+                help_text="User-visible description of the Keg.", null=True, blank=True
             ),
             preserve_default=True,
         ),
@@ -72,7 +72,7 @@ class Migration(migrations.Migration):
             model_name="kegtap",
             name="notes",
             field=models.TextField(
-                help_text=b"Private notes about this tap.", null=True, blank=True
+                help_text="Private notes about this tap.", null=True, blank=True
             ),
             preserve_default=True,
         ),
@@ -80,13 +80,13 @@ class Migration(migrations.Migration):
             model_name="keg",
             name="status",
             field=models.CharField(
-                default=b"available",
-                help_text=b"Current keg state.",
+                default="available",
+                help_text="Current keg state.",
                 max_length=32,
                 choices=[
-                    (b"available", b"Available"),
-                    (b"on_tap", b"On tap"),
-                    (b"finished", b"Finished"),
+                    ("available", "Available"),
+                    ("on_tap", "On tap"),
+                    ("finished", "Finished"),
                 ],
             ),
             preserve_default=True,
@@ -105,7 +105,7 @@ class Migration(migrations.Migration):
             name="full_volume_ml",
             field=models.FloatField(
                 default=0,
-                help_text=b"Full volume of this Keg; usually set automatically from keg_type.",
+                help_text="Full volume of this Keg; usually set automatically from keg_type.",
             ),
             preserve_default=True,
         ),
@@ -113,20 +113,20 @@ class Migration(migrations.Migration):
             model_name="keg",
             name="keg_type",
             field=models.CharField(
-                default=b"half-barrel",
-                help_text=b"Keg container type, used to initialize keg's full volume",
+                default="half-barrel",
+                help_text="Keg container type, used to initialize keg's full volume",
                 max_length=32,
                 choices=[
-                    (b"corny-2_5-gal", b"Corny Key (2.5 gal)"),
-                    (b"other", b"Other"),
-                    (b"quarter", b"Quarter Barrel (7.75 gal)"),
-                    (b"half-barrel", b"Half Barrel (15.5 gal)"),
-                    (b"euro-half", b"European Half Barrel (50 L)"),
-                    (b"corny", b"Corny Keg (5 gal)"),
-                    (b"sixth", b"Sixth Barrel (5.17 gal)"),
-                    (b"corny-3-gal", b"Corny Keg (3.0 gal)"),
-                    (b"euro-30-liter", b"European DIN (30 L)"),
-                    (b"euro", b"European Full Barrel (100 L)"),
+                    ("corny-2_5-gal", "Corny Key (2.5 gal)"),
+                    ("other", "Other"),
+                    ("quarter", "Quarter Barrel (7.75 gal)"),
+                    ("half-barrel", "Half Barrel (15.5 gal)"),
+                    ("euro-half", "European Half Barrel (50 L)"),
+                    ("corny", "Corny Keg (5 gal)"),
+                    ("sixth", "Sixth Barrel (5.17 gal)"),
+                    ("corny-3-gal", "Corny Keg (3.0 gal)"),
+                    ("euro-30-liter", "European DIN (30 L)"),
+                    ("euro", "European Full Barrel (100 L)"),
                 ],
             ),
             preserve_default=True,
@@ -134,7 +134,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="drinkingsession",
             name="timezone",
-            field=models.CharField(default=b"UTC", max_length=256),
+            field=models.CharField(default="UTC", max_length=256),
             preserve_default=True,
         ),
         migrations.RunPython(set_initial_time_zone),

08e9eddd2b57fecb2ca0efc6c17c00159b308885
mike wakerly <opensource@hoho.com>
2022-07-23 15:43:58 +0100

    Update docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,12 +61,10 @@ master_doc = "index"
 # a list of builtin themes.
 #
 html_theme = "furo"
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
+html_theme_options = {
+    "light_logo": "kegbot-logo-black.png",
+    "dark_logo": "kegbot-logo-white.png",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
